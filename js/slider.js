c0e18317e99b268247a960d35267fe7838c436e6
Simun Strukan <simun.strukan@gmail.com>
2024-09-23 23:41:01 +0200

    Added pitch page, cleaned up

---
+++ js/slider.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    const $mobileContent = $('.mobile-content');
+    let lastScrollTop = 0;
+
+    // Debounce function
+    function debounce(func, wait) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
+
+    function snapToNearestSlide() {
+        const scrollTop = $mobileContent.scrollTop();
+        const slideHeight = $(window).height();
+        const targetSlideIndex = Math.round(scrollTop / slideHeight);
+        const targetScrollTop = targetSlideIndex * slideHeight;
+
+        $mobileContent.animate({
+            scrollTop: targetScrollTop
+        }, 300);
+    }
+
+    $mobileContent.on('scroll', debounce(function() {
+        const scrollTop = $mobileContent.scrollTop();
+        const scrollDelta = Math.abs(scrollTop - lastScrollTop);
+        
+        if (scrollDelta > $(window).height() * 0.25) {
+            snapToNearestSlide();
+        }
+        
+        lastScrollTop = scrollTop;
+    }, 100));
+});
